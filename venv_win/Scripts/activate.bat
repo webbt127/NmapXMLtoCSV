e8c91ab83f820fb9d5e983d361ee59235333f43c
webbt127 <80007652+webbt127@users.noreply.github.com>
2023-03-04 03:42:25 -0600

    fix venv_win

--- venv_win/Scripts/activate.bat
+++ venv_win/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\todd_\OneDrive\Documents\GitHub\NmapXMLtoCSVx\venv_win
+set VIRTUAL_ENV=C:\Users\twebb\OneDrive - POWER Engineers, Inc\Documents\GitHub\NmapXMLtoCSV\venv_win
 
 if not defined PROMPT set PROMPT=$P$G
 
